9b7203206d46d6a8c29818baedd3c6c2c0bb0ebe
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2023-01-17 18:29:59 +0100

    basic token generation for security was added

---
+++ src/main/java/com/exadel/tenderflex/core/Constants.java
@@ -0,0 +1,6 @@
+package com.exadel.tenderflex.core;
+
+public class Constants {
+    public static final String ENCODING = "UTF-8";
+    public static final String TOKEN_HEADER = "audit_access_token";
+}
