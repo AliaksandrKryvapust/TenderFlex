ce5e6e5795a482438b14b2a8ae5e9b1c4b971dfe
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2023-01-17 17:14:54 +0100

    init

---
+++ src/main/java/com/exadel/tenderflex/TenderFlexApplication.java
@@ -0,0 +1,13 @@
+package com.exadel.tenderflex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenderFlexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TenderFlexApplication.class, args);
+    }
+
+}
