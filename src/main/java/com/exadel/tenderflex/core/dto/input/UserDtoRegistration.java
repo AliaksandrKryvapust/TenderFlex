5e304e2914c2f6a67fb3e73c9eba4d3b9dcfc842
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2023-01-19 16:49:22 +0100

    enum validation fix

--- src/main/java/com/exadel/tenderflex/core/dto/input/UserDtoRegistration.java
+++ src/main/java/com/exadel/tenderflex/core/dto/input/UserDtoRegistration.java
@@ -1,6 +1,8 @@
 package com.exadel.tenderflex.core.dto.input;
 
+import com.exadel.tenderflex.controller.validator.api.IUserRoleRegistrationSubset;
 import com.exadel.tenderflex.controller.validator.api.IValidEmail;
+import com.exadel.tenderflex.repository.entity.EUserRole;
 import lombok.Builder;
 import lombok.Data;
 import lombok.extern.jackson.Jacksonized;
@@ -23,4 +25,7 @@ public class UserDtoRegistration {
     @NotEmpty
     @IValidEmail(message = "email is not valid")
     private final String email;
+    @NotNull(message = "user role cannot be null")
+    @IUserRoleRegistrationSubset(anyOf = {EUserRole.BIDDER, EUserRole.CONTRACTOR}, message = "user role is not valid")
+    private final EUserRole role;
 }
