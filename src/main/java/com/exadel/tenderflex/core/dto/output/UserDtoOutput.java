a7c71bbf3628b2d2352ccce82c1b65901435e800
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2023-01-19 10:43:38 +0100

    user dto`s for output were added

---
+++ src/main/java/com/exadel/tenderflex/core/dto/output/UserDtoOutput.java
@@ -0,0 +1,22 @@
+package com.exadel.tenderflex.core.dto.output;
+
+import com.exadel.tenderflex.repository.entity.EUserRole;
+import com.exadel.tenderflex.repository.entity.EUserStatus;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NonNull;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Builder
+@Data
+public class UserDtoOutput {
+    private final @NonNull UUID id;
+    private final @NonNull String username;
+    private final @NonNull String email;
+    private final @NonNull EUserRole role;
+    private final @NonNull EUserStatus status;
+    private final @NonNull Instant dtCreate;
+    private final @NonNull Instant dtUpdate;
+}
