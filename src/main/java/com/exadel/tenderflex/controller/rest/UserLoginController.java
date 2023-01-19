eae16c5649b90fdf60974a244b073989f3bc1a2a
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2023-01-19 17:34:59 +0100

    UserLoginController was added

---
+++ src/main/java/com/exadel/tenderflex/controller/rest/UserLoginController.java
@@ -0,0 +1,45 @@
+package com.exadel.tenderflex.controller.rest;
+
+import com.exadel.tenderflex.core.dto.input.UserDtoLogin;
+import com.exadel.tenderflex.core.dto.input.UserDtoRegistration;
+import com.exadel.tenderflex.core.dto.output.UserDtoOutput;
+import com.exadel.tenderflex.core.dto.output.UserLoginDtoOutput;
+import com.exadel.tenderflex.manager.api.IUserManager;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@Validated
+@RequestMapping("/api/v1/users")
+public class UserLoginController {
+    private final IUserManager userManager;
+
+    public UserLoginController(IUserManager userManager) {
+        this.userManager = userManager;
+    }
+
+    @GetMapping("/me")
+    protected ResponseEntity<UserDtoOutput> getCurrentUser() {
+        UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String username = userDetails.getUsername();
+        return ResponseEntity.ok(this.userManager.getUser(username));
+    }
+
+    @PostMapping("/login")
+    protected ResponseEntity<UserLoginDtoOutput> login(@RequestBody @Valid UserDtoLogin dtoLogin) {
+        UserLoginDtoOutput userLoginDtoOutput = userManager.login(dtoLogin);
+        return ResponseEntity.ok(userLoginDtoOutput);
+    }
+
+    @PostMapping("/registration")
+    protected ResponseEntity<UserLoginDtoOutput> registration(@RequestBody @Valid UserDtoRegistration dtoInput) {
+        UserLoginDtoOutput userLoginDtoOutput = this.userManager.saveUser(dtoInput);
+        return new ResponseEntity<>(userLoginDtoOutput, HttpStatus.CREATED);
+    }
+}
