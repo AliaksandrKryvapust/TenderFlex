8dea13a5245fcd04d67ab370f1f3de900f670844
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2023-01-20 19:16:37 +0100

    refactoring: validator class was extracted

---
+++ src/main/java/com/exadel/tenderflex/service/validator/UserValidator.java
@@ -0,0 +1,26 @@
+package com.exadel.tenderflex.service.validator;
+
+import com.exadel.tenderflex.repository.entity.User;
+import com.exadel.tenderflex.service.validator.api.IUserValidator;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.OptimisticLockException;
+
+@Component
+public class UserValidator implements IUserValidator {
+
+    @Override
+    public void validateEntity(User user) {
+        if (user.getId() != null || user.getDtUpdate() != null) {
+            throw new IllegalStateException("User id should be empty");
+        }
+    }
+
+    @Override
+    public void optimisticLockCheck(Long version, User currentEntity) {
+        Long currentVersion = currentEntity.getDtUpdate().toEpochMilli();
+        if (!currentVersion.equals(version)) {
+            throw new OptimisticLockException("user table update failed, version does not match update denied");
+        }
+    }
+}
