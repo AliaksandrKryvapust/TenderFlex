b2256463b2fb24255ca2553680b8a39fb9cb7a4a
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2023-01-21 00:25:20 +0100

    refactoring: new method was extracted to validator

--- src/main/java/com/exadel/tenderflex/service/validator/api/IUserValidator.java
+++ src/main/java/com/exadel/tenderflex/service/validator/api/IUserValidator.java
@@ -1,6 +1,9 @@
 package com.exadel.tenderflex.service.validator.api;
 
+import com.exadel.tenderflex.core.dto.input.UserDtoLogin;
 import com.exadel.tenderflex.repository.entity.User;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface IUserValidator extends IValidator<User>{
+    void validateUser(UserDtoLogin userDtoLogin, UserDetails userDetails);
 }
