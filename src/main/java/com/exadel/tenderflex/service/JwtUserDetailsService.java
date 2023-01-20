a951f91edf666c2776a5a370e3e371eb7525b782
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2023-01-20 10:43:17 +0100

    get role methods fix

--- src/main/java/com/exadel/tenderflex/service/JwtUserDetailsService.java
+++ src/main/java/com/exadel/tenderflex/service/JwtUserDetailsService.java
@@ -15,6 +15,7 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -28,6 +29,7 @@ public class JwtUserDetailsService implements UserDetailsService {
     private final UserMapper userMapper;
 
     @Override
+    @Transactional(readOnly = true)
     public UserDetails loadUserByUsername(String email) {
         User user = userRepository.findByEmail(email);
         userDetailsValidator.validate(email, user);
