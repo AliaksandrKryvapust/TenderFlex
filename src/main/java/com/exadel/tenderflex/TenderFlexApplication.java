2e3fc08dc692cf44dcee7be9a9a70d2ee04cbf87
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2023-01-20 19:14:05 +0100

    refactoring: proxy method was added

--- src/main/java/com/exadel/tenderflex/TenderFlexApplication.java
+++ src/main/java/com/exadel/tenderflex/TenderFlexApplication.java
@@ -2,8 +2,10 @@ package com.exadel.tenderflex;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy(exposeProxy = true)
 public class TenderFlexApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/exadel/tenderflex/service/JwtUserDetailsService.java
+++ src/main/java/com/exadel/tenderflex/service/JwtUserDetailsService.java
@@ -4,6 +4,9 @@ import com.exadel.tenderflex.repository.api.IUserRepository;
 import com.exadel.tenderflex.repository.entity.EUserStatus;
 import com.exadel.tenderflex.repository.entity.Privilege;
 import com.exadel.tenderflex.repository.entity.User;
+import lombok.RequiredArgsConstructor;
+import org.aopalliance.aop.AspectException;
+import org.springframework.aop.framework.AopContext;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -16,23 +19,20 @@ import java.util.NoSuchElementException;
 import java.util.Set;
 
 @Service
+@RequiredArgsConstructor
 public class JwtUserDetailsService implements UserDetailsService {
     private final IUserRepository userRepository;
 
-    public JwtUserDetailsService(IUserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
-
     @Override
-    @Transactional(readOnly = true)
     public UserDetails loadUserByUsername(String email) {
-        User user = this.userRepository.findByEmail(email);
+        JwtUserDetailsService proxy = getProxy();
+        User user = proxy.getUser(email);
         this.validate(email, user);
         boolean enabled = user.getStatus().equals(EUserStatus.ACTIVATED);
         boolean nonLocked = !user.getStatus().equals(EUserStatus.DEACTIVATED);
         Set<GrantedAuthority> authorityList = new HashSet<>();
         user.getRoles().forEach((i) -> {
-            authorityList.add(new SimpleGrantedAuthority("ROLE_" + i.getRole().name()));
+            authorityList.add(new SimpleGrantedAuthority("ROLE_" + i.getRoleType().name()));
             for (Privilege privilege : i.getPrivileges()) {
                 authorityList.add(new SimpleGrantedAuthority(privilege.getPrivilege().name()));
             }
@@ -41,9 +41,22 @@ public class JwtUserDetailsService implements UserDetailsService {
                 true, true, nonLocked, authorityList);
     }
 
+    @Transactional(readOnly = true)
+    public User getUser(String email){
+        return this.userRepository.findByEmail(email);
+    }
+
     private void validate(String email, User user) {
         if (user == null) {
             throw new NoSuchElementException("There is no such user" + email);
+        }
+    }
+
+    private JwtUserDetailsService getProxy() {
+        try {
+            return (JwtUserDetailsService) AopContext.currentProxy();
+        } catch (AspectException e) {
+            return this;
         }
     }
 }
