345da79f3f38d69a75666c1f739e456d388bbc4d
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2023-01-21 23:46:38 +0100

    refactoring: Transactional was moved to repo

--- src/main/java/com/exadel/tenderflex/service/JwtUserDetailsService.java
+++ src/main/java/com/exadel/tenderflex/service/JwtUserDetailsService.java
@@ -15,7 +15,6 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.HashSet;
 import java.util.Set;
