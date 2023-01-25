adeca3be8147c4fd69e734c988624b37bcb4dd59
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2023-01-25 16:38:12 +0100

    new entity was added

---
+++ src/main/java/com/exadel/tenderflex/repository/entity/RejectDecision.java
@@ -0,0 +1,28 @@
+package com.exadel.tenderflex.repository.entity;
+
+import lombok.*;
+import org.hibernate.annotations.GenerationTime;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.time.Instant;
+import java.util.UUID;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "reject_decision", schema = "app")
+public class RejectDecision {
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "org.hibernate.id.UUIDGenerator")
+    private UUID id;
+    @Setter
+    private UUID rejectDecisionFile;
+    @org.hibernate.annotations.Generated(GenerationTime.INSERT)
+    private Instant dtCreate;
+    @Version
+    private Instant dtUpdate;
+}
