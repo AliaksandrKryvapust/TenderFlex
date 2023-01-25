9d61ab37ee65beb45f32c1ae32300c5f561b67a2
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2023-01-25 16:21:34 +0100

    enums were moved to separate package

--- src/main/java/com/exadel/tenderflex/repository/entity/EAwardCriteria.java
+++
@@ -1,5 +0,0 @@
-package com.exadel.tenderflex.repository.entity;
-
-public enum EAwardCriteria {
-    PRICE, QUALITY, LIFE_CYCLE_COSTS, ENVIRONMENT, SOCIAL_CONSIDERATIONS_AND_INNOVATION
-}
--- src/main/java/com/exadel/tenderflex/repository/entity/ETenderType.java
+++
@@ -1,5 +0,0 @@
-package com.exadel.tenderflex.repository.entity;
-
-public enum ETenderType {
-    WORK, SUPPLY, SERVICE, COMBINED_CONTRACT
-}
