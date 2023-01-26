ccdc7579958effdf663ce6a4d531d4333a2d4e44
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2023-01-26 12:25:36 +0100

    reject decision mapper was added dto`s were updated

--- src/main/java/com/exadel/tenderflex/core/dto/input/FilesDtoInput.java
+++ src/main/java/com/exadel/tenderflex/core/dto/input/FilesDtoInput.java
@@ -5,7 +5,6 @@ import lombok.Data;
 import lombok.extern.jackson.Jacksonized;
 
 import javax.validation.constraints.Future;
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.util.UUID;
@@ -17,10 +16,7 @@ public class FilesDtoInput {
     @NotNull(message = "contract deadline cannot be null")
     @Future(message = "contract deadline should refer to moment in the future")
     private final LocalDate contractDeadline;
-    @NotBlank(message = "contract file cannot be blank")
     private final UUID contractFile;
-    @NotBlank(message = "award decision file cannot be blank")
     private final UUID awardDecisionFile;
-    @NotBlank(message = "reject decision file cannot be blank")
     private final UUID rejectDecisionFile;
 }
