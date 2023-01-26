5a9baa8c6488615f7df6f16b770f8aa7dffa8db1
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2023-01-26 21:53:26 +0100

    json format for date fix

--- src/main/java/com/exadel/tenderflex/core/dto/input/FilesDtoInput.java
+++ src/main/java/com/exadel/tenderflex/core/dto/input/FilesDtoInput.java
@@ -1,5 +1,8 @@
 package com.exadel.tenderflex.core.dto.input;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
 import lombok.Builder;
 import lombok.Data;
 import lombok.extern.jackson.Jacksonized;
@@ -15,6 +18,8 @@ import java.util.UUID;
 public class FilesDtoInput {
     @NotNull(message = "contract deadline cannot be null")
     @Future(message = "contract deadline should refer to moment in the future")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
+    @JsonDeserialize(using = LocalDateDeserializer.class)
     private final LocalDate contractDeadline;
     private final UUID contractFile;
     private final UUID awardDecisionFile;
