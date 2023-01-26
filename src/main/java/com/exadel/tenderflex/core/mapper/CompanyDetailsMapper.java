c667d75063203fdd4f5d92a1f34e2238695a31a0
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2023-01-26 12:07:31 +0100

    new mappers were added

---
+++ src/main/java/com/exadel/tenderflex/core/mapper/CompanyDetailsMapper.java
@@ -0,0 +1,29 @@
+package com.exadel.tenderflex.core.mapper;
+
+import com.exadel.tenderflex.core.dto.input.CompanyDetailsDtoInput;
+import com.exadel.tenderflex.core.dto.output.CompanyDetailsDtoOutput;
+import com.exadel.tenderflex.repository.entity.CompanyDetails;
+import com.exadel.tenderflex.repository.entity.enums.ECountry;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope(value = "prototype", proxyMode = ScopedProxyMode.TARGET_CLASS)
+public class CompanyDetailsMapper {
+    public CompanyDetails inputMapping(CompanyDetailsDtoInput dtoInput){
+        return CompanyDetails.builder()
+                .officialName(dtoInput.getOfficialName())
+                .registrationNumber(dtoInput.getRegistrationNumber())
+                .country(ECountry.valueOf(dtoInput.getCountry()))
+                .town(dtoInput.getTown()).build();
+    }
+
+    public CompanyDetailsDtoOutput outputMapping(CompanyDetails companyDetails){
+        return CompanyDetailsDtoOutput.builder()
+                .officialName(companyDetails.getOfficialName())
+                .registrationNumber(companyDetails.getRegistrationNumber())
+                .country(companyDetails.getCountry().name())
+                .town(companyDetails.getTown()).build();
+    }
+}
---
+++ src/main/java/com/exadel/tenderflex/core/mapper/ContactPersonMapper.java
@@ -0,0 +1,26 @@
+package com.exadel.tenderflex.core.mapper;
+
+import com.exadel.tenderflex.core.dto.input.ContactPersonDtoInput;
+import com.exadel.tenderflex.core.dto.output.ContactPersonDtoOutput;
+import com.exadel.tenderflex.repository.entity.ContactPerson;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope(value = "prototype", proxyMode = ScopedProxyMode.TARGET_CLASS)
+public class ContactPersonMapper {
+    public ContactPerson inputMapping(ContactPersonDtoInput dtoInput){
+        return ContactPerson.builder()
+                .name(dtoInput.getName())
+                .surname(dtoInput.getSurname())
+                .phoneNumber(dtoInput.getPhoneNumber()).build();
+    }
+
+    public ContactPersonDtoOutput outputMapping(ContactPerson contactPerson){
+        return ContactPersonDtoOutput.builder()
+                .name(contactPerson.getName())
+                .surname(contactPerson.getSurname())
+                .phoneNumber(contactPerson.getPhoneNumber()).build();
+    }
+}
