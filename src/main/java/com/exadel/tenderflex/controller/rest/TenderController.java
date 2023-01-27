fc08777d3d6b409cd6b044dc96aca1fac817dcf3
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2023-01-27 22:21:45 +0100

    tender controller was updated to handle files

--- src/main/java/com/exadel/tenderflex/controller/rest/TenderController.java
+++ src/main/java/com/exadel/tenderflex/controller/rest/TenderController.java
@@ -1,10 +1,11 @@
 package com.exadel.tenderflex.controller.rest;
 
-import com.exadel.tenderflex.core.dto.input.TenderDtoInput;
 import com.exadel.tenderflex.core.dto.output.TenderDtoOutput;
 import com.exadel.tenderflex.core.dto.output.pages.PageDtoOutput;
 import com.exadel.tenderflex.core.dto.output.pages.TenderPageDtoOutput;
+import com.exadel.tenderflex.repository.entity.enums.EFileType;
 import com.exadel.tenderflex.service.api.ITenderManager;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -15,9 +16,8 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.validation.Valid;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 @RestController
@@ -39,26 +39,31 @@ public class TenderController {
         return ResponseEntity.ok(tenderManager.getDto(id));
     }
 
-    @PostMapping
-    protected ResponseEntity<TenderDtoOutput> post(@RequestBody @Valid TenderDtoInput dtoInput) {
-        return new ResponseEntity<>(this.tenderManager.saveDto(dtoInput), HttpStatus.CREATED);
-    }
-
-    @PostMapping(value = "/upload", consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
+    @PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
     protected ResponseEntity<TenderDtoOutput> postWithFile(@RequestParam(value = "tender") String tender,
                                                            @RequestParam(value = "contract", required = false) MultipartFile contract,
                                                            @RequestParam(value = "award", required = false) MultipartFile award,
                                                            @RequestParam(value = "reject", required = false) MultipartFile reject) {
-        List<MultipartFile> files = new ArrayList<>();
-        files.add(contract);
-        files.add(award);
-        files.add(reject);
-        return new ResponseEntity<>(this.tenderManager.saveFormData(tender, files), HttpStatus.CREATED);
+        Map<EFileType, MultipartFile> files = collectFiles(contract, award, reject);
+        return new ResponseEntity<>(this.tenderManager.saveDto(tender, files), HttpStatus.CREATED);
+    }
+
+    @NonNull
+    private Map<EFileType, MultipartFile> collectFiles(MultipartFile contract, MultipartFile award, MultipartFile reject) {
+        Map<EFileType, MultipartFile> files = new HashMap<>();
+        files.put(EFileType.CONTRACT, contract);
+        files.put(EFileType.AWARD_DECISION, award);
+        files.put(EFileType.REJECT_DECISION, reject);
+        return files;
     }
 
     @PutMapping("/{id}/dt_update/{version}")
     protected ResponseEntity<TenderDtoOutput> put(@PathVariable UUID id, @PathVariable(name = "version") String version,
-                                                  @Valid @RequestBody TenderDtoInput dtoInput) {
-        return ResponseEntity.ok(this.tenderManager.updateDto(dtoInput, id, Long.valueOf(version)));
+                                                  @RequestParam(value = "tender") String tender,
+                                                  @RequestParam(value = "contract", required = false) MultipartFile contract,
+                                                  @RequestParam(value = "award", required = false) MultipartFile award,
+                                                  @RequestParam(value = "reject", required = false) MultipartFile reject) {
+        Map<EFileType, MultipartFile> files = collectFiles(contract, award, reject);
+        return ResponseEntity.ok(this.tenderManager.updateDto(tender, files, id, Long.valueOf(version)));
     }
 }
