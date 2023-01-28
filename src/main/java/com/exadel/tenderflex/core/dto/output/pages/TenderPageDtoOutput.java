bf651eeee68c36c9a818843b2d9e6624a20486af
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2023-01-28 21:41:53 +0100

    tender page dto filed was changed

--- src/main/java/com/exadel/tenderflex/core/dto/output/pages/TenderPageDtoOutput.java
+++ src/main/java/com/exadel/tenderflex/core/dto/output/pages/TenderPageDtoOutput.java
@@ -2,6 +2,7 @@ package com.exadel.tenderflex.core.dto.output.pages;
 
 import com.exadel.tenderflex.core.dto.output.OfferDtoOutput;
 import com.exadel.tenderflex.core.dto.output.UserLoginDtoOutput;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NonNull;
@@ -19,6 +20,7 @@ public class TenderPageDtoOutput {
     private final @NotNull String cpvCode;
     private final @NonNull String officialName;
     private final @NotNull String tenderStatus;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
     private final @NotNull LocalDate submissionDeadline;
     private final @Nullable Set<OfferDtoOutput> offers;
     private final @Nullable Integer offersAmount;
