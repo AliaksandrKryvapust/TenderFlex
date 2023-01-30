140efe142f95ed7a7522b12f0a3f11c53edc4a1f
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2023-01-30 17:08:33 +0100

    url was changed to signed url for security reasons.

--- src/main/java/com/exadel/tenderflex/service/AwsS3Service.java
+++ src/main/java/com/exadel/tenderflex/service/AwsS3Service.java
@@ -1,38 +1,43 @@
 package com.exadel.tenderflex.service;
 
+import com.amazonaws.HttpMethod;
 import com.amazonaws.services.s3.AmazonS3;
 import com.exadel.tenderflex.core.dto.aws.AwsS3FileDto;
 import com.exadel.tenderflex.core.dto.aws.FileMetadata;
 import com.exadel.tenderflex.repository.entity.enums.EFileType;
 import com.exadel.tenderflex.service.api.IAwsS3Service;
+import com.exadel.tenderflex.service.validator.api.IAwsS3Validator;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
-import java.util.UUID;
-
-import static com.exadel.tenderflex.core.Constants.BUCKET_NAME;
+import java.util.*;
 
 @Service
 @RequiredArgsConstructor
 public class AwsS3Service implements IAwsS3Service {
+    @Value("${amazon.aws.bucket.name}")
+    private String BUCKET_NAME;
     private final AmazonS3 amazonS3;
+    private final IAwsS3Validator awsS3Validator;
 
     @Override
     public AwsS3FileDto sendFileToS3(MultipartFile file) {
         String awsFileName = UUID.randomUUID() + "." + Objects.requireNonNull(file.getContentType())
-                .substring(file.getContentType().indexOf("/")+1);
-        FileMetadata metadata = new FileMetadata(Objects.requireNonNull(file.getContentType()),file.getSize());
+                .substring(file.getContentType().indexOf("/") + 1);
+        FileMetadata metadata = new FileMetadata(Objects.requireNonNull(file.getContentType()
+                .substring(file.getContentType().indexOf("/") + 1)), file.getSize());
         try {
             amazonS3.putObject(BUCKET_NAME, awsFileName, file.getInputStream(), metadata);
         } catch (IOException e) {
             throw new IllegalStateException("Failed to send to AWS S3 file " + file.getOriginalFilename());
         }
-        String url = amazonS3.getUrl(BUCKET_NAME, awsFileName).toString();
+        Calendar calendar = generateExpirationTime();
+        awsS3Validator.validateFileKeyToStorage(BUCKET_NAME, awsFileName);
+        String url = amazonS3.generatePresignedUrl(BUCKET_NAME, awsFileName, calendar.getTime(), HttpMethod.GET).toString();
         return new AwsS3FileDto(url, awsFileName);
     }
 
@@ -50,4 +55,11 @@ public class AwsS3Service implements IAwsS3Service {
         urls.put(fileType, contract);
     }
 
+    @NonNull
+    private Calendar generateExpirationTime() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(new Date());
+        calendar.add(Calendar.DATE, 1); // Expiration time 1 day
+        return calendar;
+    }
 }
