4f0be7705126d79ea97c57b1fc730de0a3b34971
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2023-01-30 17:03:20 +0100

    bucket name was moved from constants to properties

--- src/main/java/com/exadel/tenderflex/core/Constants.java
+++ src/main/java/com/exadel/tenderflex/core/Constants.java
@@ -3,5 +3,4 @@ package com.exadel.tenderflex.core;
 public class Constants {
     public static final String ENCODING = "UTF-8";
     public static final String USER_ENTITY_GRAPH = "user.role.privileges";
-    public static final String BUCKET_NAME = "javatests3111222";
 }
