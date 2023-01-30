45cacca816c5cd274e7f36c98859374744ce3fea
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2023-01-30 19:43:46 +0100

    new File Service was created

---
+++ src/main/java/com/exadel/tenderflex/service/api/IFileManager.java
@@ -0,0 +1,7 @@
+package com.exadel.tenderflex.service.api;
+
+import com.exadel.tenderflex.core.dto.input.FileDtoInput;
+import com.exadel.tenderflex.core.dto.output.FileDtoOutput;
+
+public interface IFileManager extends IManagerUpdate<FileDtoOutput, FileDtoInput> {
+}
