190d13c7580830a3f85c959978a3b592b3b1a6ea
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2023-01-30 23:30:56 +0100

    new method to get page of tenders for current user was added

--- src/main/java/com/exadel/tenderflex/repository/api/ITenderRepository.java
+++ src/main/java/com/exadel/tenderflex/repository/api/ITenderRepository.java
@@ -1,9 +1,15 @@
 package com.exadel.tenderflex.repository.api;
 
 import com.exadel.tenderflex.repository.entity.Tender;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.UUID;
 
 public interface ITenderRepository extends JpaRepository<Tender, UUID> {
+    @Query("select t from Tender t where t.user.email=:email")
+    Page<Tender> findAllForUser(@Param("email") String email, Pageable pageable);
 }
