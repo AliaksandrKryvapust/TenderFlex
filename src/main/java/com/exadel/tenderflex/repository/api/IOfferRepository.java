63abb6363a3b57688e06e5288c6ccc0f6d2bdbcf
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2023-02-01 15:36:37 +0100

    new method was added

--- src/main/java/com/exadel/tenderflex/repository/api/IOfferRepository.java
+++ src/main/java/com/exadel/tenderflex/repository/api/IOfferRepository.java
@@ -1,9 +1,15 @@
 package com.exadel.tenderflex.repository.api;
 
 import com.exadel.tenderflex.repository.entity.Offer;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.UUID;
 
 public interface IOfferRepository extends JpaRepository<Offer, UUID> {
+    @Query("select o from Offer o where o.user.email=:email")
+    Page<Offer> findAllForUser(@Param("email") String email, Pageable pageable);
 }
