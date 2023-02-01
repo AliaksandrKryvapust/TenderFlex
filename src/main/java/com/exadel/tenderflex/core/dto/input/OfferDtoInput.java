835bf206e24632bef628bd6068ba9e20ecdc975b
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2023-02-01 12:58:39 +0100

    tender Id field was added

--- src/main/java/com/exadel/tenderflex/core/dto/input/OfferDtoInput.java
+++ src/main/java/com/exadel/tenderflex/core/dto/input/OfferDtoInput.java
@@ -1,10 +1,7 @@
 package com.exadel.tenderflex.core.dto.input;
 
 import com.exadel.tenderflex.controller.validator.api.IValidEnum;
-import com.exadel.tenderflex.repository.entity.CompanyDetails;
-import com.exadel.tenderflex.repository.entity.ContactPerson;
 import com.exadel.tenderflex.repository.entity.enums.ECurrency;
-import com.exadel.tenderflex.repository.entity.enums.EOfferStatus;
 import lombok.Builder;
 import lombok.Data;
 import lombok.extern.jackson.Jacksonized;
@@ -20,17 +17,16 @@ import javax.validation.constraints.Positive;
 public class OfferDtoInput {
     @Valid
     @NotNull(message = "company details cannot be null")
-    private final CompanyDetails bidder;
+    private final CompanyDetailsDtoInput companyDetails;
     @Valid
     @NotNull(message = "contact person cannot be null")
-    private final ContactPerson contactPerson;
+    private final ContactPersonDtoInput contactPerson;
     @NotNull(message = "bid price cannot be null")
     @Positive(message = "bid price cannot be negative")
     private final Integer bidPrice;
     @NotNull(message = "currency cannot be null")
     @IValidEnum(enumClass = ECurrency.class, message = "currency does not match")
     private final String currency;
-    @NotNull(message = "proposition file cannot be null")
-    @NotBlank(message = "proposition file cannot be blank")
-    private final String propositionFile;
+    @NotBlank(message = "tender is not selected")
+    private final String tenderId;
 }
