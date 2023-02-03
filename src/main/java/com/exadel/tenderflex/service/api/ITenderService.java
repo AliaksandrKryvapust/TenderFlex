5c0350cde9fbc694bc8c414116deba384d76c528
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2023-02-03 21:44:00 +0100

    new methods to get all were added

--- src/main/java/com/exadel/tenderflex/service/api/ITenderService.java
+++ src/main/java/com/exadel/tenderflex/service/api/ITenderService.java
@@ -1,9 +1,9 @@
 package com.exadel.tenderflex.service.api;
 
-import com.exadel.tenderflex.repository.entity.Offer;
 import com.exadel.tenderflex.repository.entity.Tender;
-
-import java.util.UUID;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface ITenderService extends IService<Tender>, IServiceUpdate<Tender> {
+    Page<Tender> getAll(Pageable pageable);
 }
